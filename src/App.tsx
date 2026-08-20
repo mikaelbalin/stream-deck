6a224810466a1a5106169ed6fda18d2bddcdf4bc
Mikael Balin <hello@mikaelbalin.com>
2026-08-20 22:36:29 +0100

    Enable transparent window with rounded corners and update app title

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
 
 	return (
 		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-			<div className="flex min-h-screen flex-col bg-accent">
+			<div className="flex min-h-screen flex-col bg-accent overflow-hidden rounded-2xl">
 				<Titlebar connected={connected} />
 				<Header connected={connected} />
 				<main className="flex flex-1 items-center justify-center p-6">
